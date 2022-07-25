7493507a341e3008a0d72b4fbb4565f2e04a725d
magnicarelle <carelledjeukeu@gmail.com>
2022-07-25 13:42:09 +0200

    mon premier dom

---
+++ dom.js
@@ -0,0 +1,8 @@
+function f() {
+    document.getElementById ("ma").innerHTML ="salut la famille";
+    document.getElementById ("ma").style.color ="red";
+    document.getElementById ("ma").style.background ="#0000ffff";
+    document.getElementById ("ma").style.fontSize ="52px";
+    document.getElementById ("ma").style.letterSpacing ="12px";
+
+}
